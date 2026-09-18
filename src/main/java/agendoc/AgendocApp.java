5b4e9b634201f6eb378e4406694dfdbf568f2e7e
Michael Pozhidaev <msp@luwrain.org>
2026-09-18 13:19:49 +0700

    Fixing the main window layout

--- src/main/java/agendoc/AgendocApp.java
+++ src/main/java/agendoc/AgendocApp.java
@@ -7,6 +7,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -38,10 +41,30 @@ public class AgendocApp extends Application
         ListView<String> listView = new ListView<>();
         listView.getItems().addAll("Item 1", "Item 2", "Item 3");
 
-        VBox vbox = new VBox(10, llmButton, textArea, textField, listView);
-        vbox.setPadding(new Insets(10));
+        VBox mainBox = new VBox(10, llmButton, textArea, textField);
+        VBox.setVgrow(textArea, Priority.ALWAYS);
 
-        Scene scene = new Scene(vbox, 600, 400);
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(10));
+
+        ColumnConstraints mainColumn = new ColumnConstraints();
+        mainColumn.setPercentWidth(75);
+
+        ColumnConstraints listColumn = new ColumnConstraints();
+        listColumn.setPercentWidth(25);
+
+        grid.getColumnConstraints().addAll(mainColumn, listColumn);
+        grid.add(mainBox, 0, 0);
+        grid.add(listView, 1, 0);
+
+        GridPane.setHgrow(mainBox, Priority.ALWAYS);
+        GridPane.setHgrow(listView, Priority.ALWAYS);
+        GridPane.setVgrow(mainBox, Priority.ALWAYS);
+        GridPane.setVgrow(listView, Priority.ALWAYS);
+
+        Scene scene = new Scene(grid, 600, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
