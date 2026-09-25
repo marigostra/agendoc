3697db42114dac7190c889e2f7625efde39f8b63
Michael Pozhidaev <msp@luwrain.org>
2026-09-25 21:06:31 +0700

    Using old classpath loader

--- src/main/java/agendoc/AgendocApp.java
+++ src/main/java/agendoc/AgendocApp.java
@@ -225,6 +225,8 @@ public class AgendocApp extends Application
      */
     public static void main(String[] args)
     {
+	if (args.length == 1 && args[0].equalsIgnoreCase("--console"))
+	    new ConsoleChat().run();
         launch(args);
     }
 }
